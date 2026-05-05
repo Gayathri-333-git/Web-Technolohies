946722ec4b02f52884e83b5b6b894020808772b1
Gayathri-333-git <gayathrim9078@gmail.com>
2026-05-05 15:27:25 +0530

    Add files via upload

---
+++ file1.js
@@ -0,0 +1,17 @@
+//we can access outside and inside variable values
+{
+var a=10
+console.log(`The value of a is `+" "+a)
+var a=78
+
+}
+console.log(`The value of a is `+" "+a)
+
+// let b=67
+// console.log(`The value of a is `+" "+b)
+// let b=89
+// console.log(`The value of a is `+" "+b)
+
+//It is not modifiable
+const c=45
+console.log(c)
